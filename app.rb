39f1728f65ef733a8206421487f499cdb12801be
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-16 00:30:20 -0300

    Sending emails now

--- app.rb
+++ app.rb
@@ -12,11 +12,30 @@ get '/hello/:name' do
 end
 
 get '/style.css' do
-  sass :'sass/style', :style => :compressed
+  sass :'sass/style', style: :compressed
 end
 
-post '/form' do  
-  "You said '#{params[:message]}'"  
+post '/contact' do 
+  require 'pony'
+  Pony.mail(
+    name: params[:name],
+    mail: params[:mail],
+    body: params[:body],
+    to: 'gustavoguichard@gmail.com',
+    subject: params[:name] + " entrou em contato pelo site",
+    body: params[:message],
+    port: '587',
+    via: :smtp,
+    via_options: {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: ENV['EMAIL_USERNAME'],
+      password: ENV['EMAIL_PASSWORD'],
+      authentication: :plain,
+      domain: 'mguichard.com.br'
+    })
+  redirect '/'
 end
 
 not_found do
